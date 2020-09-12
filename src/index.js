3afb45853a63b4d8638267a55f0f69193acc00cf
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-12 11:46:41 +0300

    create a simple React app with component state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class App extends React.Component {
+/*
+As a quick review, here’s how this works:
+- The `state` is stored in the `Counter` component
+- [Whenever] state was [changes],
+  React re-renders the Counter component (and its children),
+  [using the new state].
+*/
+class Counter extends React.Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
-    return <div className="container">Hello World!</div>;
+    return (
+      <div className="container">
+        <h2>Container</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
