fbb4a9eea97a8bba2b4798853d3c27719b78b249
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-12 11:37:41 +0300

    refactor the functional component into a class-based component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const App = () => {
-  return <div className="container">Hello World!</div>;
-};
+class App extends React.Component {
+  render() {
+    return <div className="container">Hello World!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
